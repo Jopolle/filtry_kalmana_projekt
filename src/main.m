b78a0969cd79d616038ccc3d71283ee3e9c9c0c0
Jakub Bulski <jakubbulski03@gmail.com>
2026-03-10 00:55:42 +0100

    Identyfikacja parametru K metoda LS i walidacja na wykresach z danymi pomiarowymi

--- src/main.m
+++ src/main.m
@@ -2,7 +2,7 @@ clc;
 clear;
 close all; 
 
-%% Zczytywanie danych z pliku csv do tabeli 
+%% Importing data from csv file
 filename = fullfile('..','data', 'group_08.csv');
 opts = detectImportOptions(filename, ...
     'Delimiter', ';', ...
@@ -15,11 +15,41 @@ h1      = T{:,3};
 h2      = T{:,4};
 q2_meas = T{:,5};
 
-%% Identyfikacja parametrów modelu 
-noise_only = q2_meas(1:50);         % Część czysto szumowa
-E_hat = mean(noise_only);           % Estymata średniej szumu pomiarowego
-sig_hat =  var(noise_only);         % Estymata wariancji szumu pomiarowego
+%% Identification of model parameters
+% Noise parameters
+noise_only = q2_meas(1:50);         
+E_hat = mean(noise_only);           % Estimate of noise mean value
+sig_hat =  var(noise_only);         % Estimate of noise variance
 noise_std  = std(noise_only);
-fprintf('Średnia szumu = %.6f\n', E_hat); 
-fprintf('Wariancja szumu = %.6f\n', sig_hat);
-fprintf('Odchylenie standardowe szumu = %.6f\n', noise_std);
+fprintf('Noise mean value = %.6f\n', E_hat); 
+fprintf('Noise variance = %.6f\n', sig_hat);
+fprintf('Standard deviation = %.6f\n', noise_std);
+
+% 'K' parameter identification (LS)
+idx = 51:length(t);          
+numerator   = sum(sqrt(h2(idx)) .* q2_meas(idx));
+denominator = sum(h2(idx));
+k_hat = numerator / denominator;
+fprintf('K_hat = %.6e\n', k_hat);
+
+% Model output for estimated k
+q2_model = k_hat * sqrt(h2(idx));
+
+% Validation of parameter K value on figures
+figure;
+plot(t(idx), q2_meas(idx), 'b', 'LineWidth', 1.2); hold on;
+plot(t(idx), q2_model, 'r--', 'LineWidth', 1.5);
+grid on;
+xlabel('Time');
+ylabel('Outlet flow q_2');
+legend('Measured q_2', 'Model: k\_hat \cdot sqrt(h_2)', 'Location', 'best');
+title('Measured and modeled q2 flow in time');
+
+figure;
+plot(sqrt(h2(idx)), q2_meas(idx), 'bo'); hold on;
+plot(sqrt(h2(idx)), q2_model, 'r.', 'MarkerSize', 12);
+grid on;
+xlabel('sqrt(h_2)');
+ylabel('Outlet flow q_2');
+legend('Measurements', 'Model', 'Location', 'best');
+title('Relationship between q_2 and sqrt(h_2)');
