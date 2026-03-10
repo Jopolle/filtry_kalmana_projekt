d91187d586b23cea8390cf857c4bb3e8ad5cba40
Jakub Bulski <jakubbulski03@gmail.com>
2026-03-10 16:21:56 +0100

    Implementacja identyfikacji parametrów S1, S2, alpha, k metodą LS

--- src/main.m
+++ src/main.m
@@ -40,21 +40,45 @@ fprintf('K_hat = %.6e\n', k_hat);
 q2_model = k_hat * sqrt_h2;
 
 % Validation of parameter K value on figures
-figure;
-subplot(2,1,1)
-plot(t(idx), q2_meas(idx), 'b', 'LineWidth', 1.2); hold on;
-plot(t(idx), q2_model, 'r--', 'LineWidth', 1.5);
-grid on;
-xlabel('Time');
-ylabel('Outlet flow q_2');
-legend('Measured q_2', 'Model: k\_hat \cdot sqrt(h_2)', 'Location', 'best');
-title('Measured and modeled q2 flow in time');
-subplot(2,1,2)
-plot(sqrt_h2, q2_meas(idx), 'bo'); hold on;
-plot(sqrt_h2, q2_model, 'r.', 'MarkerSize', 12);
-grid on;
-xlabel('sqrt(h_2)');
-ylabel('Outlet flow q_2');
-legend('Measurements', 'Model', 'Location', 'best');
-title('Relationship between q_2 and sqrt(h_2)');
+% figure;
+% subplot(2,1,1)
+% plot(t(idx), q2_meas(idx), 'b', 'LineWidth', 1.2); hold on;
+% plot(t(idx), q2_model, 'r--', 'LineWidth', 1.5);
+% grid on;
+% xlabel('Time');
+% ylabel('Outlet flow q_2');
+% legend('Measured q_2', 'Model: k\_hat \cdot sqrt(h_2)', 'Location', 'best');
+% title('Measured and modeled q2 flow in time');
+% subplot(2,1,2)
+% plot(sqrt_h2, q2_meas(idx), 'bo'); hold on;
+% plot(sqrt_h2, q2_model, 'r.', 'MarkerSize', 12);
+% grid on;
+% xlabel('sqrt(h_2)');
+% ylabel('Outlet flow q_2');
+% legend('Measurements', 'Model', 'Location', 'best');
+% title('Relationship between q_2 and sqrt(h_2)');
 
+% 'α', 'S_2' parameters identification (LS)
+idx2 = 52:(length(t)-1);
+dh2 = ((h2(idx2+1)-h2(idx2-1))/(0.5));      % h2 derivates vector
+Y = dh2(:);
+sqrt_h2 = sqrt(max(h2(idx2), 0));
+Phi = [h1(idx2)-h2(idx2), -sqrt_h2];
+Theta_hat = (Phi' * Phi) \ (Phi' * Y);
+a1_hat = Theta_hat(1);
+a2_hat = Theta_hat(2);
+
+S2_hat = k_hat / a2_hat;
+alpha_hat = a1_hat * S2_hat;
+
+fprintf('S2_hat = %.6e\n', S2_hat);
+fprintf('alpha_hat = %.6e\n', alpha_hat);
+
+% 'S_1' parameter identification (LS)
+dh1 = (h1(idx2+1) - h1(idx2-1)) / 0.5;
+Y = dh1(:);
+Psi = q1(idx2) - alpha_hat * (h1(idx2) - h2(idx2));
+Psi = Psi(:);
+a3_hat = (Psi' * Y) / (Psi' * Psi);
+S1_hat = 1 / a3_hat;
+fprintf('S1_hat = %.6e\n', S1_hat);
